1e4908f106eb81f22fee28e938ba9814c530ccf7
Henry <henry.zelonka@student.tdsb.on.ca>
2024-03-28 11:28:12 -0400

    Initial Commit Initial Commit

---
+++ src/mainFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class mainFrame extends JFrame{
+    public void init(){
+
+        setTitle("popup window :) :)");
+        setSize(600,300);
+
+        setDefaultCloseOperation
+        (EXIT_ON_CLOSE);
+
+        setVisible(true);
+
+    }
+}
